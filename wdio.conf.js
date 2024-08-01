011cdeade0346e0dde93df86ee5acc80b9874f03
abighailshafira <abighailshafira@upi.edu>
2024-08-01 17:48:13 +0700

    Add Allure reporter configuration for test reporting

--- wdio.conf.js
+++ wdio.conf.js
@@ -124,7 +124,7 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: https://webdriver.io/docs/dot-reporter
-  reporters: ["spec"],
+  reporters: ["spec", ["allure", { outputDir: "allure-results" }]],
 
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {
